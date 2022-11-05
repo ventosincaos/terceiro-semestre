36e63cabee54528e37f345f201aa15f5b16a0b3a
ventosincaos <111576277+ventosincaos@users.noreply.github.com>
2022-11-05 20:57:22 -0300

    quarto commit

--- Linguagem de Programacao/formulario/script01.php
+++ Linguagem de Programacao/formulario/script01.php
@@ -31,11 +31,50 @@
         $pessoas["telefone"] = $_GET["telefone"];
         $pessoas["email"] = $_GET["email"];
 
-        $linha = json_encode ($pessoas);
-        $arq = fopen ("script00","a".PHP_EOL);
-        $y = fwrite($arq,$linha);
-        $y = fclose($arq);
-    ?>
+        $linha = json_encode ($pessoas).chr(13).chr(10);
+        $arq=fopen("formulario.txt","a");
+        fwrite($arq,$linha);
+        fclose($arq);    
+        ?>
 
-</body>
-</html>
+        <style>
+                    #arquivo {
+                    font-family: Arial, Helvetica, sans-serif;
+                    border-collapse: collapse;
+                    width: 100%;
+                    }
+                    
+                    #arquivo td, #arquivo th {
+                    border: 1px solid #ddd;
+                    padding: 8px;
+                    }
+                    
+                    #customers th {
+                    padding-top: 12px;
+                    padding-bottom: 12px;
+                    text-align: left;
+                    background-color: #04AA6D;
+                    color: white;
+                    }
+    </style>
+    <table  id="arquivo">
+    <?php
+
+    $arquivo = fopen("formulario.txt", "r");
+
+    echo "<tr><td>Nome</td><td>Sexo</td><td>Data de Nascimento</td><td>Telefone</td><td>E-mail</td></tr>";
+
+    while(!feof($arquivo)) {
+      $linha = json_decode(fgets($arquivo),true);
+      if ($linha !=null) {echo"<tr>";
+     foreach($linha as $info){
+         echo "<td>".$info."</td>";
+     }
+      echo"</tr>";
+    
+    }}
+    echo "</table>";
+    fclose($arquivo);
+    ?>
+    </body>
+    </html>
